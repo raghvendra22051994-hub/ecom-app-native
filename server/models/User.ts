d2aa68ceb76d69d901e38348c1bb08c533345143
Raghvendra <raghvendra22051994@gmail.com>
2026-02-22 23:39:29 +0530

    first commit

---
+++ server/models/User.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import { IUser } from "../types/index.js";
+import { User } from "@clerk/express";
+
+const userSchema = new mongoose.Schema<IUser>(
+    {
+        name: { type: String, trim: true },
+        email: { type: String, unique: true, trim: true },
+        clerkId: { type: String, unique: true, trim: true },
+        image: { type: String },
+        role: { type: String, enum: ["user", "admin"], default: "user" },
+    },
+    { timestamps: true }
+);
+
+const User = mongoose.model<IUser>("User", userSchema);
+
+export default User;
---
+++ server/server.ts
@@ -0,0 +1,25 @@
+import "dotenv/config";
+import express, { Request, Response } from 'express';
+import cors from "cors";
+import connectDB from "./config/db.js";
+import { clerkMiddleware } from '@clerk/express'
+
+const app = express();
+
+// Middleware
+app.use(cors())
+app.use(express.json());
+app.use(clerkMiddleware())
+
+// Connect to MongoDB
+connectDB();
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Server is Live!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
